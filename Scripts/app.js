9bad4e0998c5fad5de730fe836663188439c5ca1
llllQ <matthew.penlington@gmail.com>
2019-06-09 13:55:44 +0100

    hamburger animation

--- Scripts/app.js
+++ Scripts/app.js
@@ -3,6 +3,7 @@ const navLinks = document.querySelector('.nav-links');
 const navItem = document.querySelectorAll('.nav-item');
 
 hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('is-active');
     navLinks.classList.toggle('open');
     navItem.forEach(link =>{
         link.classList.toggle('fade');
